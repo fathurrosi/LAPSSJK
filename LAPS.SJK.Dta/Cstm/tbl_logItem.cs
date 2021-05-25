0fc5a13ca12d7bcd363173ba60e4c754738aa925
fathur rosi <rosi.fathur@gmail.com>
2021-05-25 23:26:56 +0700

    init dto

---
+++ LAPS.SJK.Dta/Cstm/tbl_logItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAPS.SJK.Dta
+{
+    public partial class tbl_LogItem
+    {
+    }
+}
