b5b05b827ecff61e21877f08ba386be947372eca
fathur rosi <rosi.fathur@gmail.com>
2021-05-26 20:47:42 +0700

    implement multi language to master page

--- LAPS.SJK.Logic/Helper/LanguageHelper.cs
+++ LAPS.SJK.Logic/Helper/LanguageHelper.cs
@@ -23,25 +23,57 @@ namespace LAPS.SJK.Logic.Helper
 
         public static MvcHtmlString LangSwitcher(this UrlHelper url, string name, RouteData routeData, string lang)
         {
-            var liTagBuilder = new TagBuilder("li");
+            //var liTagBuilder = new TagBuilder("li");
             //liTagBuilder.Attributes.Add("class", classAttribute);
             var aTagBuilder = new TagBuilder("a");
+            aTagBuilder.AddCssClass("dropdown-item");
             var routeValueDictionary = new RouteValueDictionary(routeData.Values);
+
+            //< a class="dropdown-item" href="#"><img src = "@Url.Content("~/Content/img/blank.gif")" class="flag flag-id" alt="Indonesia" /> ID</a>
+            TagBuilder imgTagBuilder = new TagBuilder("img");
+
+
+
+            if (name.ToLower() == "Indonesia".ToLower())
+            {
+                imgTagBuilder.InnerHtml = (" ID");
+                imgTagBuilder.AddCssClass("flag flag-id");
+            }
+            else if (name.ToLower() == "English".ToLower())
+            {
+                imgTagBuilder.InnerHtml = (" EN");
+                imgTagBuilder.AddCssClass("flag flag-us");
+
+            }
+            imgTagBuilder.Attributes.Add("alt", name);
+            imgTagBuilder.MergeAttribute("src", "/Content/img/blank.gif");
+
             if (routeValueDictionary.ContainsKey("lang"))
             {
-                if (routeData.Values["lang"] as string == lang)
-                {
-                    liTagBuilder.AddCssClass("active");
-                }
-                else
-                {
-                    routeValueDictionary["lang"] = lang;
-                }
+                //if (routeData.Values["lang"] as string == lang)
+                //{
+                //    liTagBuilder.AddCssClass("active");
+                //}
+                //else
+                //{
+                routeValueDictionary["lang"] = lang;
+                //}
             }
             aTagBuilder.MergeAttribute("href", url.RouteUrl(routeValueDictionary));
             aTagBuilder.SetInnerText(name);
-            liTagBuilder.InnerHtml = aTagBuilder.ToString();
-            return new MvcHtmlString(liTagBuilder.ToString());
+            // liTagBuilder.InnerHtml = aTagBuilder.ToString();
+
+
+            //var culture = System.Threading.Thread.CurrentThread.CurrentUICulture.Name.ToLowerInvariant();
+            //var imgSrc = string.Format("/assets/images/flags/{0}.png", culture);
+            //TagBuilder tag = new TagBuilder("img");
+            //tag.AddCssClass("flag-lang");
+            //tag.MergeAttribute("src", imgSrc);
+            //return MvcHtmlString.Create(tag.ToString(TagRenderMode.SelfClosing));
+
+
+            aTagBuilder.InnerHtml = (imgTagBuilder.ToString());
+            return new MvcHtmlString(aTagBuilder.ToString());
         }
     }
 }
